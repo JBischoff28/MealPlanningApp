2592ac42dacb093d0f7637ea4cd0a37c97178164
Joshua Bischoff <joshuabischoff1@gmail.com>
2023-03-20 20:56:11 -0400

    Started new app named dishes. This app will be used to link dishes to users and meals in the database. Created serializer for the Dish model. Added dishes app to the settings. Ran migrations to translate the Dish model to the database.

---
+++ backend/dishes/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from authentication.models import User
+from meals.models import Meal
+
+class Dish(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
