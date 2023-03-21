0aca30954b65701133c01e7a50901ba319f0bcaf
Joshua Bischoff <joshuabischoff1@gmail.com>
2023-03-20 21:21:38 -0400

    Started writing CRUD functionality for the dishes app. MUST finish later, before Postman testing.

--- backend/dishes/views.py
+++ backend/dishes/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from .models import Dish
+from .serializers import DishSerializer
 
-# Create your views here.
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def display_all_dishes(request):
+    dishes = Dish.objects.all()
+    serializer = DishSerializer(dishes)
+    return Response(data=serializer.data)
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def user_dishes(request):
+    if request.method == 'GET':
+        dishes = Dish.objects.filter(user=request.user.id)
+        serializer = DishSerializer(dishes, many=True)
+        return Response(data=serializer.data)
+    elif request.method == 'POST':
+        serializer = DishSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
