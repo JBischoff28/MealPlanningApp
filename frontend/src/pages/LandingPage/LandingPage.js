3493cca5734e56b935e8419a391571982588336e
Joshua Bischoff <joshuabischoff1@gmail.com>
2023-03-29 17:51:34 -0400

    Finished core development of landing page. The page is marked to be completed later after main application pages are developed.

--- frontend/src/pages/LandingPage/LandingPage.js
+++ frontend/src/pages/LandingPage/LandingPage.js
@@ -22,25 +22,45 @@ const LandingPage = (props) => {
         <div className='landingContainer'>
             <Navbar />
             <div className='pageBody'>
-                <div className='container'>
-                    <div className='headerContainer'>
-                        <div className='headerText'>
-                            <h2>What is mealCONTROL?</h2>
-                        </div>
+                <div className='headerContainer'>
+                    <div className='headerText'>
+                        <h2>What is mealCONTROL?</h2>
                     </div>
                 </div>
                 <div className='aboutSection'>
                     <ul>
                         <li>
                             <div className='aboutContainer'>
+                                <div className='aboutTitle'>
+                                    <h2>Easy Decisions</h2>
+                                </div>
+                                <div className='aboutText'>
+                                    <ul>
+                                        <li>
+                                            <p>Let us help you decide what to eat!</p>
+                                        </li>
+                                        <li>
+                                            <p>mealCONTROL helps speed up the decision making process by giving you meal suggestions</p>
+                                        </li>
+                                        <li>
+                                            <p>You give us keywords, we give you meals!</p>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
                         </li>
                         <li>
                             <div className='aboutContainer'>
+                                <div className='aboutTitle'>
+                                    <h2>Meal Builder</h2>
+                                </div>
                             </div>
                         </li>
                         <li>
                             <div className='aboutContainer'>
+                                <div className='aboutTitle'>
+                                    <h2>Date Your Meals</h2>
+                                </div>
                             </div>
                         </li>
                     </ul>
