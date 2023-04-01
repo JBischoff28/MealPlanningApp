60d92401361f575744fd88b8db6629b6a710f4e5
Joshua Bischoff <joshuabischoff1@gmail.com>
2023-04-01 19:17:52 -0400

    Added a 'recipes' state variable that will be responsible for holding the list of recipes to be displayed. setRecipes function updates the state variable. Added an async function that will return a list of 20 random recipes from the Edamam API. Called async function within useEffect so random recipes will be generated whenever the user returns to the home page.

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -10,23 +10,20 @@ const HomePage = () => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
   const [user, token] = useAuth();
-  /*const [cars, setCars] = useState([]);
+  const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
-    const fetchCars = async () => {
-      try {
-        let response = await axios.get("http://127.0.0.1:8000/api/cars/", { ------------------> EXAMPLE
-          headers: {
-            Authorization: "Bearer " + token,
-          },
-        });
-        setCars(response.data);
-      } catch (error) {
-        console.log(error.response.data);
-      }
-    };
-    fetchCars();
-  }, [token]); */
+    /**fetchRecipes();**/
+  }, []);
+
+  async function fetchRecipes() {
+    try {
+      let response = await axios.get(`https://api.edamam.com/api/recipes/v2?type=public&q=well%20rounded&app_id=${app_id}&app_key=${app_key}&random=true`);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
 
   return (
@@ -34,6 +31,7 @@ const HomePage = () => {
       <HomeNavbar />
       <div className="homePageBody">
         <p>this is the home page.</p>
+        <button onClick={fetchRecipes}>Run Test - fetchRecipes</button>
       </div>
     </div>
   );
