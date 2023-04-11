7344ea0eead55e251665e2bf7e2ac599285b14d2
Joshua Bischoff <joshuabischoff1@gmail.com>
2023-04-10 21:09:11 -0400

    Mionor change to urls.py file in the meals app.

--- backend/meals/urls.py
+++ backend/meals/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.display_all_meals),
     path('mymeals/', views.user_meals),
     path('mymeal/<int:pk>/', views.user_singleMeal),
-    path('mymeal/<int:meal_pk>/mydish/<int:dish_pk>', views.meal_dish)
+    path('mymeal/<int:dish_pk>/<int:meal_pk>/', views.meal_dish)
 ]
