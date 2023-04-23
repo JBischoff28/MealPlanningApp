e63f4fe45ec1aeaa5cf887bc3f9489b3add6092a
Joshua Bischoff <joshuabischoff1@gmail.com>
2023-04-22 21:02:02 -0400

    Added styling to various buttons in the application.

--- frontend/src/components/FunFacts/FunFacts.jsx
+++ frontend/src/components/FunFacts/FunFacts.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
 const FunFacts = () => {
-    
+
     return (
         <div>
             <h3>Give a keyword and press "GO!" to generate a random meal!</h3>
             <div className='funFactContainer'>
-
                 <h4>Fun Fact #1</h4>
                 <p>Applesauce was the first food eaten in space!</p>
                 <h4>Fun Fact #2</h4>
@@ -15,7 +14,7 @@ const FunFacts = () => {
                 <p>Cucumbers are 95% water!</p>
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default FunFacts;
